37c734598b7475bdfb10d05b2356874286d4cf5b
Eddie-Ward <edwardliew1998@gmail.com>
2022-10-20 14:16:37 -0700

    Implement search bar

--- src/functions/fetch.js
+++ src/functions/fetch.js
@@ -1,21 +1,45 @@
+import { movies } from "./SeedMovies";
+
 export default function FetchData(params) {
 	return [
 		{
-			name: 'The Dark Knight',
-			image:
-				'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg',
+			name: "The Dark Knight",
+			image: "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg",
 			description:
-				'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
-			contentRating: 'PG-13',
-			genre: ['Action', 'Crime', 'Drama'],
-			datePublished: '2008-07-18',
+				"When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+			contentRating: "PG-13",
+			genre: ["Action", "Crime", "Drama"],
+			datePublished: "2008-07-18",
 			trailer: {
 				thumbnail: {
 					contentUrl:
-						'https://m.media-amazon.com/images/M/MV5BNWJkYWJlOWMtY2ZhZi00YWM0LTliZDktYmRiMGYwNzczMTZhXkEyXkFqcGdeQXVyNzU1NzE3NTg@._V1_.jpg',
+						"https://m.media-amazon.com/images/M/MV5BNWJkYWJlOWMtY2ZhZi00YWM0LTliZDktYmRiMGYwNzczMTZhXkEyXkFqcGdeQXVyNzU1NzE3NTg@._V1_.jpg",
 				},
-				url: 'https://www.imdb.com/video/vi324468761/',
+				url: "https://www.imdb.com/video/vi324468761/",
 			},
 		},
 	];
+}
+
+export async function fetchAllMovies() {
+	try {
+		if (!movies) {
+			throw new Error("No movies in collection");
+		}
+		return movies;
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+export async function fetchMovie(name) {
+	try {
+		const movie = movies.find((movie) => movie[1].jsonnnob.name === name);
+		if (!movie) {
+			throw new Error("Movie not found");
+		}
+		return movie;
+	} catch (err) {
+		console.error(err);
+	}
 }
