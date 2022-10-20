16d97be0d7efaf92a4ae602dba4a1e34c4a9f0fc
JonGoneWrong <jongonewrong@gmail.com>
2022-10-20 17:38:20 -0500

    Rebase commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,16 +2,8 @@ import React from "react";
 
 function Footer() {
 	return (
-		<div
-			style={{
-				position: "fixed",
-				bottom: "0",
-				height: "2rem",
-				display: "flex",
-				gap: "2rem",
-				alignItems: "center",
-			}}>
-			<p>Copyright</p>
+		<div className="footer">
+			Copyright
 			<a href="https://github.com/vlados552">Vladislav</a>
 			<a href="https://github.com/vlados552">Jon</a>
 			<a href="https://github.com/vlados552">Edward</a>
