52ff96c858e130cdcd640a0adfd1667e3b78d7e2
Edward Liew <110881795+Eddie-Ward@users.noreply.github.com>
2022-10-21 11:31:07 -0700

    Revert "New fetchTrendingMovies function"

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,5 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import Footer from "./components/Footer/Footer";
-import Navbar from "./components/Navbar/Navbar";
-import LibraryPage from "./components/pages/LibraryPage";
-import MainPage from "./components/pages/MainPage";
-import SpecificMoviePage from "./components/pages/SpecificMoviePage";
-import "./styles/App.css"
-
 function App() {
-	return (
-		<div className="app">
-			<Navbar />
-				<Routes>
-					<Route path="/" element={<MainPage />}></Route>
-					<Route path="/library" element={<LibraryPage />}></Route>
-					<Route path="/library/:movieID" element={<SpecificMoviePage />}></Route>
-				</Routes>
-			<Footer />
-		</div>
-	);
+	return <div className="App">Hello World</div>;
 }
 
 export default App;
