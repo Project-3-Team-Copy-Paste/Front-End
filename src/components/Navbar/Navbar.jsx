4c951778482a7b18eb142c3366fe2eb3305b26ca
Eddie-Ward <edwardliew1998@gmail.com>
2022-10-24 17:27:54 -0700

    Add registration form

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
-import React, { useMemo, useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import LoginForm from '../shared/LoginForm';
-import SearchBar from './SearchBar';
+import React, { useMemo, useState } from "react";
+import { NavLink } from "react-router-dom";
+import LoginForm from "../shared/LoginForm";
+import RegisterForm from "../shared/RegisterForm";
+import SearchBar from "./SearchBar";
 
 function Navbar({ token, setToken }) {
-	const [openModal, setOpenModal] = useState(false);
+	const [openLogin, setOpenLogin] = useState(false);
+	const [openRegister, setOpenRegister] = useState(false);
 
 	const username = useMemo(() => {
 		if (token) {
@@ -13,11 +15,12 @@ function Navbar({ token, setToken }) {
 	}, [token]);
 
 	return (
-		<div className='navBar'>
-			<div className='navLinks'>
-				<NavLink to='/' className={'reelsLink'}>
+		<div className="navBar">
+			<div className="navLinks">
+				<NavLink to="/" className={"reelsLink"}>
 					Reels
 				</NavLink>
+<<<<<<< HEAD
 				{localStorage.getItem('username') ? (
 					<>
 						<NavLink to='/watchlist' className={'watchlistLink'}>
@@ -28,6 +31,14 @@ function Navbar({ token, setToken }) {
 						</NavLink>
 					</>
 				) : null}
+=======
+				<NavLink to="/watchlist" className={"watchlistLink"}>
+					Watchlist
+				</NavLink>
+				<NavLink to="/journal" className={"journalLink"}>
+					Journal
+				</NavLink>
+>>>>>>> 38000ce (Add registration form)
 			</div>
 			<SearchBar />
 			{token ? (
@@ -45,10 +56,10 @@ function Navbar({ token, setToken }) {
 				</>
 			) : (
 				<>
-					<button onClick={() => setOpenModal(true)}>Login</button>
-					{openModal ? (
-						<LoginForm setModal={setOpenModal} setToken={setToken} />
-					) : null}
+					<button onClick={() => setOpenLogin(true)}>Login</button>
+					<button onClick={() => setOpenRegister(true)}>Register</button>
+					{openLogin ? <LoginForm setModal={setOpenLogin} setToken={setToken} /> : null}
+					{openRegister ? <RegisterForm setModal={setOpenRegister} /> : null}
 				</>
 			)}
 		</div>
